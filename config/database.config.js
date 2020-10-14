ea20e69d83f154bf92f9c803dcec52d397268407
saudkhan <saud.alphasquad@gmail.com>
2020-10-14 15:02:34 +0500

    db link changed fromlocal to wan

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,5 @@
 module.exports = {
     // url: 'mongodb://localhost:27017/easy-notes'
-    url: 'mongodb://localhost:27017/admin'
+    // url: 'mongodb://localhost:27017/admin'
+    url: `mongodb+srv://admin:393939Sk@cluster0.7cbuw.mongodb.net/test?retryWrites=true&w=majority`
 }
