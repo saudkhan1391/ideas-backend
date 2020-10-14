a7f58a3b6fa27ffa91e35d35f1a00ae138e98da6
saudkhan <saud.alphasquad@gmail.com>
2020-10-14 15:13:01 +0500

    done

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose.connect(dbConfig.url, {
 }).then(() => {
     console.log("Successfully connected to the database");
 }).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
+    console.log('Could not connect to the database(). Exiting now...', err);
     process.exit();
 });
 
